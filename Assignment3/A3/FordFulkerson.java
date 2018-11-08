07bc74a2e6da5b745264c7cc926de22add3c8bde
alexhale <a.hale@live.ca>
2018-11-08 10:12:20 -0500

    Assignment 3 submitted

--- Assignment3/A3/FordFulkerson.java
+++ Assignment3/A3/FordFulkerson.java
@@ -4,7 +4,6 @@
 
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.TimeUnit;	// TODO remove
 
 public class FordFulkerson {
 
@@ -13,88 +12,54 @@ public class FordFulkerson {
 	public static ArrayList<Integer> pathDFS(Integer source, Integer destination, WGraph graph){
 		ArrayList<Integer> stack = new ArrayList<Integer>();						// path from source to destination
 		ArrayList<Integer> discovered = new ArrayList<Integer>();					// list of nodes that have been found, but whose neighbours haven't yet been discovered
-		ArrayList<Integer> neighboursDiscovered = new ArrayList<Integer>();			// list of nodes whose neighbours have been discovered
+		ArrayList<Integer> neighboursDiscovered = new ArrayList<Integer>();			// list of nodes whose neighbours have been discovered\
+		Map<Integer, Integer> predecessors = new HashMap<Integer, Integer>();			// predecessor of each node found. Format: (a node, that node's predecessor)
 		stack.add(source);															// first element in the path
 		discovered.add(source);
 		int current;
 
 		while (!stack.isEmpty()) {
-			System.out.println("stack: " + stack.toString());		// TODO remove
-			System.out.println("discovered: " + discovered.toString());		// TODO remove
-			System.out.println("neighboursDiscovered: " + neighboursDiscovered.toString());		// TODO remove
-
 			// peek the last element of the stack (i.e. the element whose neighbours are waiting to be explored)
 			current = stack.get(stack.size() - 1);
-			stack.remove(stack.indexOf(current));
 
 			// if this node's neighbours haven't been discovered, we'd better discover them!
 			if (!neighboursDiscovered.contains(current)) {
 				// list to store the neighbours of the currently selected node
 				ArrayList<Integer> neighbours = new ArrayList<Integer>();
 
-				// look at all the edges connecting to the currently selected node
+				// look at all the edges
 				for (Edge e : graph.getEdges()) {
-					// determine whether the new node is stored in this edge's nodes[0] or nodes[1]
-					int secondNode;
-					if (e.nodes[0] == current) {
-						secondNode = e.nodes[1];
-					} else {
-						secondNode = e.nodes[0];
+
+					// if there's an edge leaving from current, that edge has non-zero weight, and the other node hasn't been discovered already
+					if (e.nodes[0] == current && !discovered.contains(e.nodes[1]) && e.weight != 0) {
+						neighbours.add(e.nodes[1]);				// add it to the list of neighbours
+						discovered.add(e.nodes[1]);				// also add it to the list of nodes we've found
+
+						predecessors.put(e.nodes[1], current);	// add predecessor to this map
 					}
-
-					// if there is an adjacent node whose neighbours haven't yet been discovered and that edge has non-zero weight
-					if (!discovered.contains(secondNode) && graph.getEdge(current, secondNode) != null && graph.getEdge(current, secondNode).weight != 0) {
-						neighbours.add(secondNode);				// add it to the list of neighbours
-						discovered.add(secondNode);				// also add it to the list of nodes we've found
-					}
-				}
-
-				if (neighbours.contains(destination)) {
-					// if we've reached the destination, add it to the stack
-					stack.add(destination);
-
-					// remove any nodes that we found but didn't explore their neighbours, because they're not on the path
-					// TODO replace
-					for (Integer i : discovered) {
-						if (!neighboursDiscovered.contains(i)) {
-							stack.remove(stack.indexOf(i));
-						}
-					}
-
-					// the stack contains the path we followed from source -> destination, and no other nodes
-					return stack;
-				} else {
-					stack.addAll(neighbours);
 				}
 
 				// we've discovered all of this node's neighbours
 				neighboursDiscovered.add(current);
-			}
-		}
 
-		/*ArrayList<Integer> stack = new ArrayList<Integer>();		// path from source to destination
-		ArrayList<Integer> visited = new ArrayList<Integer>();		// 
-		stack.add(source);
-		int current;
-
-		while (!stack.isEmpty()) {
-			// TimeUnit.SECONDS.sleep(1);
-
-			current = stack.get(stack.size() - 1);
-			stack.remove(stack.indexOf(current));
-
-			if (!visited.contains(current)) {
-				System.out.println(current + " ");
-				visited.add(current);
-			}
-
-			// look at all the edges connecting to the currently selected node
-			for (Edge e : graph.getEdges()) {
-				if (e.nodes[0] == current && !visited.contains(e.nodes[1]) && e.weight != 0) {
-					stack.add(e.nodes[1]);
+				// if we've found the destination
+				if (neighbours.contains(destination)) {
+					// find the path we followed from source to destination
+					ArrayList<Integer> path = new ArrayList<Integer>();
+					int selected = destination;
+					path.add(destination);
+					while (selected != source) {
+						selected = predecessors.get(selected);
+						path.add(0, selected);
+					}
+					return path;
+				} else {
+					stack.addAll(neighbours);
 				}
 			}
-		}*/
+
+			stack.remove(stack.indexOf(current));
+		}
 
 		// if no path is found to the destination, return an empty ArrayList
 		return new ArrayList<Integer>();
@@ -115,16 +80,15 @@ public class FordFulkerson {
 
 			// while there's a path from the source to the sink, keep augmenting the flow
 			ArrayList<Integer> path = pathDFS(source, destination, gf);
-			while (path != new ArrayList<Integer>()) {
-				// System.out.println(path.toString());; 				// TODO remove
-
+			// while (path != new ArrayList<Integer>()) {
+			while (path.size() > 0) {
 				// find the smallest (i.e. restrictive) capacity on the path
-				int smallestCapacity = 0;
+				int smallestCapacity = Integer.MAX_VALUE;
 				for (int i = 1; i < path.size(); i++) {
 					int firstNode = path.get(i - 1);
 					int secondNode = path.get(i);
 
-					if (gf.getEdge(firstNode, secondNode).weight > smallestCapacity) {
+					if (gf.getEdge(firstNode, secondNode).weight < smallestCapacity) {
 						smallestCapacity = gf.getEdge(firstNode, secondNode).weight;
 					}
 				}
@@ -140,7 +104,6 @@ public class FordFulkerson {
 					graph.setEdge(firstNode, secondNode, smallestCapacity);
 					gf.setEdge(firstNode, secondNode, (gf.getEdge(firstNode, secondNode).weight - smallestCapacity));
 				}
-
 
 				// refresh path for next iteration
 				path = pathDFS(source, destination, gf);
