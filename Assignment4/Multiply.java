721bc4f490e531f540ff090a69b8a2e842ba4c17
halestorm962 <a.hale@live.ca>
2018-11-21 12:54:41 -0500

    assignment 4 complete

--- Assignment4/Multiply.java
+++ Assignment4/Multiply.java
@@ -1,3 +1,7 @@
+// Name: Alex Hale
+// ID: 260672475
+// Collaborators: none
+
 import java.util.*;
 import java.io.*;
 
@@ -8,20 +12,66 @@ public class Multiply{
         int maxval = (1 << size) - 1;
         return rand.nextInt(maxval + 1);
     }
-    
-    public static int[] naive(int size, int x, int y) {
 
-        // YOUR CODE GOES HERE  (Note: Change return statement)
-        
-        return null;
-        
+    // size = bit length of integers x and y
+    // return result[0] = result of multiplication, result[1] = # of brute force operations undertaken
+    public static int[] naive(int size, int x, int y) {      
+        int[] result = new int[2];
+    	int operations = 0;
+
+    	if (size == 1) {
+    		result[0] = x * y;
+    		result[1] = 1;			// multiplication of two 1-bit numbers costs 1 operation
+    	} else {
+    		// implement divide-and-conquer naive multiplication algorithm
+    		int m = (int) Math.ceil((double) size / (double) 2);
+    		int pow = (int) Math.pow((double) 2, (double) m);
+			int a = x / pow;
+			int b = x % pow;
+			int c = y / pow;
+			int d = y % pow;
+			int[] e = naive(m, a, c);
+			int[] f = naive(m, b, d);
+			int[] g = naive(m, b, c);
+			int[] h = naive(m, a, d);
+
+			result[0] = (int) (Math.pow((double) 2, (double) 2*m) * e[0]) + (pow * (g[0] + h[0])) + f[0];
+			
+			// naive algorithm costs 3 operations times the size of the integers being multiplied
+			result[1] = e[1] + f[1] + g[1] + h[1] + (3 * m);
+    	}
+
+        return result;
     }
 
+    // size = bit length of integers x and y
+    // return result[0] = result of multiplication, result[1] = # of brute force operations undertaken
     public static int[] karatsuba(int size, int x, int y) {
+    	int[] result = new int[2];
         
-        // YOUR CODE GOES HERE  (Note: Change return statement)
+        if (size == 1) {
+        	result[0] = x * y;
+        	result[1] = 1;			// multiplication of two 1-bit numbers costs 1 operation
+        } else {
+        	// implement divide-and-conquer karatsuba multiplication algorithm
+        	int m = (int) Math.ceil((double) size / (double) 2);   	
+        	int pow = (int) Math.pow((double) 2, (double) m);		
+        	int a = x / pow; 				            			
+        	int b = x % pow;			 							
+        	int c = y / pow;			 							
+        	int d = y % pow;			 							
+
+        	int[] e = karatsuba(m, a, c);
+        	int[] f = karatsuba(m, b, d);
+        	int[] g = karatsuba(m, a - b, c - d);
+
+        	result[0] = (int) (Math.pow((double) 2, (double) 2*m) * e[0]) + (pow * (e[0] + f[0] - g[0])) + f[0];
+        	
+        	// naive algorithm costs 6 operations times the size of the integers being multiplied
+        	result[1] = e[1] + f[1] + g[1] + (6 * m);
+        }
         
-        return null;
+        return result;
         
     }
     
